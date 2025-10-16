89e8a92d911ef46567483a2ea6d40f9081ec3e72
Xin Yu Zhang <xyzhang2022@gmail.com>
2025-10-16 08:59:56 -0400

    week 3/4 hw added to own folder

---
+++ hw-week3_4/src/App.js
@@ -0,0 +1,9 @@
+// import ButtonPage from './pages/ButtonPage'
+// import AccordionPage from './pages/AccordionPage'
+import CarouselPage from './pages/CarouselPage'
+
+const App = () => {
+  return <CarouselPage />
+}
+
+export default App
